8d106b02977631dc3aca56bbef74c3cec7d5baeb
“huzaifacoder” <huzaifashaikhpython@gmail.com>
2023-09-15 11:42:39 +0400

    rerouted the streamlit run to home_page.py

--- running_main_st.py
+++ running_main_st.py
@@ -2,12 +2,12 @@ import os
 
 
 def streamlit_cmd():
-    # Change directory to the location of Reference.py
-    script_dir = os.path.dirname(os.path.realpath("pages/Reference.py"))
+    # Change directory to the location of 2_📑_Reference.py
+    script_dir = os.path.dirname(os.path.realpath("pages/1_👋_Home_Page.py"))
     os.chdir(script_dir)
 
-    # Run the Streamlit app using streamlit run Reference.py with --no-reload option
-    os.system("streamlit run Reference.py")
+    # Run the Streamlit app using streamlit run 2_📑_Reference.py with --no-reload option
+    os.system("streamlit run 1_👋_Home_Page.py")
 
 if __name__ == "__main__":
     streamlit_cmd()
